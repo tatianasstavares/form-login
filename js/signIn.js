fa2905c29e83b7eba6c7a2b8b3c1a4658b44341a
Tatiana <tatianasstavares@gmail.com>
2021-07-10 16:05:09 +0100

    fix: remove event listener from button

--- js/signIn.js
+++ js/signIn.js
@@ -1,8 +1,3 @@
 const inputEmail = document.querySelector(".input-email");
 const inputPassword = document.querySelector(".input-password");
 const loginButton = document.querySelector(".button-submit");
-
-loginButton.addEventListener("click", () => {
-  alert(`Email : ${inputEmail.value} 
-Senha: ${inputPassword.value}`);
-});
